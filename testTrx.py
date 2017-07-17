5a723942cafd941252c907a778b73d0dcb6211aa
kurokid <kuro.kid@gmail.com>
2017-07-17 15:01:15 +0700

    Cleanup code

--- testTrx.py
+++ testTrx.py
@@ -1,5 +1,5 @@
 """
-(C) Copyright 2009 Igor V. Custodio
+(C) Copyright 2009 kurokid
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
